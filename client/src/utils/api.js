c9f0efbabe86e6dd5544b84d69f573a80450e1da
PRiteshal <riteshpal337@gmail.com>
2026-06-07 23:25:44 +0530

    update api url for production

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API = axios.create({ baseURL: "/api" });
+const API = axios.create({ baseURL: "https://healthcare-backend-fe0g.onrender.com/api" });
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
